9c74221a0f3e7bc2bcdf00be62538127841a1cc3
zhu <zxxhu002@odu.edu>
2019-03-07 11:51:53 -0500

    new b

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,7 +6,9 @@
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    std::cout << "Hello World!\n";
+	std::cout << "sf";
+	std::cout <<"new brach"
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
